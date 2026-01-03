84d241446046bf6fda908b56a58336bd94ac5dca
michaelharoldsony <michaelharoldsony.dev@gmail.com>
2026-01-03 22:55:26 +0530

    Modified Files

--- app/src/main/java/com/example/connexus/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/connexus/navigation/AppNavGraph.kt
@@ -1,2 +1,37 @@
 package com.example.connexus.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.*
+import com.example.connexus.ui.auth.LoginScreen
+import com.example.connexus.ui.auth.SignupScreen
+import com.example.connexus.ui.home.HomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onLoginSuccess = { navController.navigate("home") },
+                onSignupClick = { navController.navigate("signup") }
+            )
+        }
+
+        composable("signup") {
+            SignupScreen(
+                onSignupSuccess = { navController.navigate("home") }
+            )
+        }
+
+        composable("home") {
+            HomeScreen()
+        }
+    }
+}
